94edefe6de813cc7ff097829612d6545cf69f3f8
Tayyip Çakmak <tayyipcakmak@hotmail.com.tr>
2020-10-12 17:17:38 +0300

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Tayyip Çakmak on 12.10.2020.
+//  Copyright © 2020 Tayyip Cakmak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
